cc370789ed5bb2ad00c57801a40c8a0023241bb7
lucasfcosta <fernandesdacostalucas@gmail.com>
2017-04-07 23:37:59 -0300

    Move components folder to src folder

--- index.ios.js
+++ index.ios.js
@@ -2,7 +2,7 @@
 // You'll be free hackers
 // You'll be free hackers ooooooooooooooo
 
-import App from './components/App';
+import App from './src/components/App';
 import { AppRegistry } from 'react-native';
 
 AppRegistry.registerComponent('picster', () => App);
