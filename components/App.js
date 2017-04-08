1d1aa30a3d238f567a1f460fc384f328ab29ca77
lucasfcosta <fernandesdacostalucas@gmail.com>
2017-04-07 23:34:46 -0300

    More linting stuff

--- components/App.js
+++ components/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import firebase from 'firebase';
 import { Text } from 'react-native';
+
+const config = {
+  apiKey: "AIzaSyBqCmZMQt76m87ftuxXEv-tGyftCCOVWXw",
+  authDomain: "picster-18f83.firebaseapp.com",
+  databaseURL: "https://picster-18f83.firebaseio.com",
+  projectId: "picster-18f83",
+  storageBucket: "picster-18f83.appspot.com",
+  messagingSenderId: "218259833332"
+};
+
+firebase.initializeApp(config);
 
 const App = () => (
   <Text>Picster</Text>
