32be2523258fbd0a56456387a5652e08c110450c
Lucas Vieira <vieiralucas4@gmail.com>
2017-04-08 00:20:14 -0300

    Fix style Login.js

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import firebase from 'firebase';
 import { Button, View } from 'react-native';
 
 import * as colors from '../colors'
 
-const Login = () => {
-
-  return (
-    <View>
-      <Button title='Login With Facebook'
-        color={colors.blue}
-      />
-    </View>
-  )
-}
-
-const style = {
-}
+const Login = () => (
+  <View>
+    <Button title='Login With Facebook'
+      color={colors.blue}
+    />
+  </View>
+)
 
 export default Login
